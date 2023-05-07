fda5d9504ed56506d7807b382afb19d060aa7915
Basic-Funda-Developer <100834471+codes-guide-youtube@users.noreply.github.com>
2023-05-07 20:09:22 +0530

    addition of title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return ( <div>
+    <Head>
+      <title> Payment Tracker </title>
+    </Head>
           <h1>Hello World</h1>
     </div>
   )
