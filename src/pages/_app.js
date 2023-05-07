aeb54d79ac2a0552aa86a7f719e2cc75cd6dfe71
Basic-Funda-Developer <100834471+codes-guide-youtube@users.noreply.github.com>
2023-05-07 20:04:35 +0530

    inclusion of footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
-
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div className="min-h-screen mx-auto container"> 
+  <Header />
+  <Component {...pageProps} />
+    <Footer className="bottom-0" />
+  </div>
 }
